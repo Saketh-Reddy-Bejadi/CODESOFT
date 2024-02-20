f021e1f139d66ada7f44240eb6f9f849fb9d5ce3
Saketh Reddy Bejadi <bejadisakethreddy168@gmail.com>
2024-02-20 22:58:15 +0530

    done with all the 3 task

--- Calculator/src/scripts.js
+++ Calculator/src/scripts.js
@@ -1,36 +1,22 @@
-// let displayValue = "";
+let displayValue = "";
 
 function appendToDisplay(value) {
   displayValue += value;
   document.getElementById("display").value = displayValue;
 }
 
-// function clearEntry() {
-//   var display = document.getElementById("display");
-//   display.value = display.value.slice(0, -1); // Remove the last character
-// }
-
-// function allClear() {
-//   document.getElementById('display').value = ''; // Clear the entire display
-// }
-
-let displayValue = ""; // Variable to store the current value displayed on the screen
-
-// Function to handle AC button click
 function handleAC() {
-  displayValue = ""; // Clear the display value
-  updateDisplay(); // Update the display
+  displayValue = ""; 
+  updateDisplay(); 
 }
 
-// Function to handle C button click
 function handleC() {
-  displayValue = displayValue.slice(0, -1); // Remove the last character from the display value
-  updateDisplay(); // Update the display
+  displayValue = displayValue.slice(0, -1); 
+  updateDisplay(); 
 }
 
-// Function to update the display with the current value
 function updateDisplay() {
-  document.getElementById("display").value = displayValue; // Update the display with the current value
+  document.getElementById("display").value = displayValue; 
 }
 
 function calculate() {
